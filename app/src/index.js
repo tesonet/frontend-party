eabe247ed51146483a9c2e602d415d2548700086
Dovydas Ceilutka <dovydas@troublemaker.xyz>
2016-11-30 09:18:09 +0200

    Add Bootstrap CSS and fix the <head> 🤕

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,26 @@
-// Set up your application entry point here...
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import Match from 'react-router/Match';
+import Miss from 'react-router/Miss';
+import BrowserRouter from 'react-router/BrowserRouter';
+import configureStore from './store/configureStore';
+import './style.scss';
+
+const store = configureStore();
+
+const Placeholder = () => (
+  <div>Placeholder</div>
+);
+
+render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Placeholder} />
+        <Match pattern="/login" component={Placeholder} />
+        <Miss component={Placeholder} />
+      </div>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'),
+);
