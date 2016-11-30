b61f17ba85561b344bd2f4f80832b9da1fddd48f
Dovydas Ceilutka <dovydas@troublemaker.xyz>
2016-11-30 22:14:49 +0200

    Updated testSetup to fake localStorage for tests

--- app/tools/testSetup.js
+++ app/tools/testSetup.js
@@ -19,3 +19,10 @@ process.env.NODE_ENV = 'test';
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
 require('babel-register')();
+
+// XXX: Should be a better way to do this
+// eslint-disable-next-line
+localStorage = {
+  getItem: () => 'token',
+  removeItem: () => null,
+};
