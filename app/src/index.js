d51a966387e7b62e258c0b5a62cb1c48dfc9def4
Dovydas Ceilutka <dovydas@troublemaker.xyz>
2016-11-30 22:47:42 +0200

    Update routes

--- app/src/index.js
+++ app/src/index.js
@@ -4,22 +4,23 @@ import { Provider } from 'react-redux';
 import Match from 'react-router/Match';
 import Miss from 'react-router/Miss';
 import BrowserRouter from 'react-router/BrowserRouter';
-import configureStore from './store/configureStore';
 import './style.scss';
+import { signinUserWithToken } from './actions/authentication';
+import configureStore from './store/configureStore';
+import Login from './pages/Login';
+import Main from './pages/Main';
+import NotFound from './pages/NotFound';
 
 const store = configureStore();
-
-const Placeholder = () => (
-  <div>Placeholder</div>
-);
+store.dispatch(signinUserWithToken());
 
 render(
   <Provider store={store}>
     <BrowserRouter>
-      <div>
-        <Match exactly pattern="/" component={Placeholder} />
-        <Match pattern="/login" component={Placeholder} />
-        <Miss component={Placeholder} />
+      <div className="layout">
+        <Match exactly pattern="/" component={Main} />
+        <Match pattern="/login" component={Login} />
+        <Miss component={NotFound} />
       </div>
     </BrowserRouter>
   </Provider>, document.getElementById('root'),
