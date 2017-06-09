5859bf8f1f183099b93465de7121218bb429c75b
Martin Saulis <martin@saulis.com>
2017-06-09 10:08:04 +0300

    remove redundant named import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Routes from './Routes';
-import globalStyles from './globalStyles.scss'; // eslint-disable-line no-unused-vars
+import './globalStyles.scss';
 
 if (__DEV__) {
   window.store = store;
