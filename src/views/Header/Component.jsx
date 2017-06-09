63109b0605615848363d6c2eee984423994c125f
Martin Saulis <martin@saulis.com>
2017-06-09 10:22:55 +0300

    simplify logout button click handler

--- src/views/Header/Component.jsx
+++ src/views/Header/Component.jsx
@@ -3,27 +3,21 @@ import cn from 'classnames';
 
 import styles from './styles.scss';
 
-class Header extends React.Component {
-  handleLogoutClick = () => this.props.logout();
-
-  render() {
-    return (
-      <div className={cn('row', styles.header)}>
-        <div className={cn('col-9 col-xs-12', styles.main)}>
-          <img
-            src="/static/img/logo.png"
-            alt="Testio"
-            width={115}
-            height={30}
-            className={styles.logo}
-          />
-        </div>
-        <div className={cn('col-3 col-xs-12', styles.aside)}>
-          <button onClick={this.handleLogoutClick}>&#x21e4; Logout</button>
-        </div>
-      </div>
-    );
-  }
-}
+const Header = props => (
+  <div className={cn('row', styles.header)}>
+    <div className={cn('col-9 col-xs-12', styles.main)}>
+      <img
+        src="/static/img/logo.png"
+        alt="Testio"
+        width={115}
+        height={30}
+        className={styles.logo}
+      />
+    </div>
+    <div className={cn('col-3 col-xs-12', styles.aside)}>
+      <button onClick={props.logout}>&#x21e4; Logout</button>
+    </div>
+  </div>
+);
 
 export default Header;
