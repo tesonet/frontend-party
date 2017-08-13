8bae60971694a10620d4ae0250ef96983890bab5
Mindaugas Jacionis <m.jacionis@gmail.com>
2017-08-13 04:43:35 +0300

    Adds basic render tests

---
+++ client/src/__tests__/components.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Button, Input } from '../components/ui';
+import { ServerRow } from '../components/servers';
+import { LoginForm } from '../components/login';
+
+test('Basic button renders without crash', () => {
+  const component = renderer.create(
+    <Button onClick={() => console.log('I was clicked')}/>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the onClick
+  tree.props.onClick();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Basic Input renders without crash', () => {
+  const component = renderer.create(
+    <Input />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Basic ServerRow renders without crash', () => {
+  const component = renderer.create(
+    <ServerRow />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('Basic LoginForm renders without crash', () => {
+  const component = renderer.create(
+    <LoginForm />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
