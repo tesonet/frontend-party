1f2905d542db81c8d56235def5051a341f995ba5
Marius Alchimavicius <alch.marius@gmail.com>
2017-09-08 20:32:55 +0300

    init project

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Container, Col, Row } from 'reactstrap';
+// import styles from './index.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <Row>
+          <Col xs="3">
+            labas
+          </Col>
+          <Col xs="3">
+            labas
+          </Col>
+          <Col xs="3">
+            labas
+          </Col>
+          <Col xs="3">
+            labas
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export { Login };
