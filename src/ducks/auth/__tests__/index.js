33f9be591ac24b69dc49973bd1cbafef04fab7ca
Marius Alchimavicius <alch.marius@gmail.com>
2017-09-11 18:50:25 +0300

    consistent test structure

--- src/ducks/auth/__tests__/index.js
+++ src/ducks/auth/__tests__/index.js
@@ -7,38 +7,42 @@ import { push } from 'react-router-redux';
 const mockStore = configureStore();
 const axiosMock = new AxiosMock(api);
 
-it('should login user', async () => {
-  const token = 'ABCDEFGH';
+describe('async actions', () => {
+  
+  it('should login user', async () => {
+    const token = 'ABCDEFGH';
+  
+    axiosMock
+      .onPost('/api/tokens')
+      .reply(200, { token });
+  
+    const store = mockStore({});
+    initApi(store);
+  
+    expect(localStorage.getItem('token')).toEqual(null);
+  
+    await actions.login('user', 'pass')(store.dispatch);
+    const expectedActions = [
+      actions.registerToken(token),
+      push('/servers'),
+    ];
+  
+    expect(localStorage.getItem('token')).toEqual(token);
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+  
+  it('should logout user', async () => {
+    const store = mockStore({});
+  
+    actions.logout()(store.dispatch);
+  
+    const expectedActions = [
+      actions.registerToken(null),
+      push('/login'),
+    ];
+  
+    expect(store.getActions()).toEqual(expectedActions);
+    expect(localStorage.getItem('token')).toEqual(null);
+  });
+})
 
-  axiosMock
-    .onPost('/api/tokens')
-    .reply(200, { token });
-
-  const store = mockStore({});
-  initApi(store);
-
-  expect(localStorage.getItem('token')).toEqual(null);
-
-  await actions.login('user', 'pass')(store.dispatch);
-  const expectedActions = [
-    actions.registerToken(token),
-    push('/servers'),
-  ];
-
-  expect(localStorage.getItem('token')).toEqual(token);
-  expect(store.getActions()).toEqual(expectedActions);
-});
-
-it('should logout user', async () => {
-  const store = mockStore({});
-
-  actions.logout()(store.dispatch);
-
-  const expectedActions = [
-    actions.registerToken(null),
-    push('/login'),
-  ];
-
-  expect(store.getActions()).toEqual(expectedActions);
-  expect(localStorage.getItem('token')).toEqual(null);
-});
