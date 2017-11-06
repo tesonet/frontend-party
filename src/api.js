b02361dbc0ed0b3d3d909b449cf949c6214fee0b
gytautas <gytautas.pilkauskas@gmail.com>
2017-11-06 09:57:58 +0200

    tesonet api calls and theme

---
+++ src/api.js
@@ -0,0 +1,28 @@
+import request, { serialize } from './utils/request';
+
+export const API_BASE_URL = 'http://playground.tesonet.lt/v1';
+export const API_TOKEN_URL = `${API_BASE_URL}/tokens`;
+export const API_SERVER_LIST_URL = `${API_BASE_URL}/servers`;
+
+export const getToken = (username, password) =>
+  request(API_TOKEN_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: serialize({ username, password }),
+  });
+
+
+export const getServerList = (token) =>
+  request(API_SERVER_LIST_URL, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+export default {
+  getToken,
+  getServerList,
+};
---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export default {
+  primaryTextColor: '#fff',
+  primaryBgColor: '#fff',
+
+  secondaryTextColor: '#b2b2b2',
+  secondaryBgColor: '#f5f5f5',
+
+  borderColor: '#e5e5e5',
+
+  btnPrimaryBgColor: '#9fd533',
+  btnPrimaryHoverBgColor: '#99cc33',
+  btnDisabledBgColor: '#b2b2b2',
+  btnSecondaryTextColor: '#2f3254',
+  inputActiveTextColor: '#999',
+
+  listTextColor: '#666666',
+  listHeadTextColor: '#999',
+};
