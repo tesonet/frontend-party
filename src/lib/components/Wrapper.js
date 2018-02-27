eb92124971bf7ace93b171b9921a973734c612e5
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2018-02-27 23:52:33 +0200

    Use full page width for servers screen

--- src/lib/components/Wrapper.js
+++ src/lib/components/Wrapper.js
@@ -5,9 +5,7 @@ import { Col, Grid, Row } from 'react-bootstrap';
 const Wrapper = ({ children, className, ...rest }) => (
   <Grid fluid className={className}>
     <Row>
-      <Col xs={12} md={10} mdOffset={1} {...rest}>
-        {children}
-      </Col>
+      <Col {...rest}>{children}</Col>
     </Row>
   </Grid>
 );
