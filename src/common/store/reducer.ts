53f19abdfc8104bddb50953b8d55a2fc5b559ab2
throwaway1227 <42039550+throwaway1227@users.noreply.github.com>
2018-08-12 20:23:25 +0300

    remove dummy state

--- src/common/store/reducer.ts
+++ src/common/store/reducer.ts
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import authReducer from './modules/auth/reducer';
 
 const reducer = combineReducers({
-  device: (state = { test: true }) => state,
   auth: authReducer
 });
 
