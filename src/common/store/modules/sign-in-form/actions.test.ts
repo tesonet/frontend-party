72a8b33bec632d2119ecc26c49f19c875720083a
throwaway1227 <42039550+throwaway1227@users.noreply.github.com>
2018-08-13 15:06:11 +0300

    add sign in tests

--- src/common/store/modules/sign-in-form/actions.test.ts
+++ src/common/store/modules/sign-in-form/actions.test.ts
@@ -1,8 +1,11 @@
 jest.mock('./validation');
+jest.mock('./api');
 
 import configureStore from 'common/store/configure-store';
-import { validateField } from './actions';
-import { validateUsername } from './validation';
+import { signIn, validateField } from './actions';
+import { fetchToken } from './api';
+import { Status } from './types';
+import { validatePassword, validateUsername } from './validation';
 
 describe('sign-in-form actions', () => {
   describe('#validateField', () => {
@@ -35,7 +38,7 @@ describe('sign-in-form actions', () => {
 
       const error = new Error('test error');
 
-      (validateUsername as any).mockImplementation(() => {
+      (validateUsername as jest.Mock).mockImplementation(() => {
         throw error;
       });
 
@@ -44,6 +47,80 @@ describe('sign-in-form actions', () => {
 
       expect(nextState.isValid).toEqual(false);
       expect(nextState.error).toEqual(error);
+    });
+  });
+
+  describe('#signIn', () => {
+    it('should set auth flags to true, when fields are valid and request succeeds', async () => {
+      const store = configureStore({
+        signInForm: {
+          fields: {
+            password: { value: 'test-password', isValid: true, error: null },
+            username: { value: 'test-username', isValid: true, error: null }
+          }
+        }
+      });
+
+      const token = 'test-token';
+      (validateUsername as jest.Mock).mockImplementation();
+      (validatePassword as jest.Mock).mockImplementation();
+      (fetchToken as jest.Mock).mockImplementation(() => ({
+        token
+      }));
+
+      await store.dispatch(signIn());
+      const nextState = store.getState();
+
+      expect(nextState.auth.token).toEqual(token);
+      expect(nextState.auth.isAuthenticated).toEqual(true);
+      expect(nextState.signInForm.status).toEqual(Status.Idle);
+    });
+
+    it('should set error when fields are valid and request fails', async () => {
+      const store = configureStore({
+        signInForm: {
+          fields: {
+            password: { value: 'test-password', isValid: true, error: null },
+            username: { value: 'test-username', isValid: true, error: null }
+          }
+        }
+      });
+
+      (validateUsername as jest.Mock).mockImplementation();
+      (validatePassword as jest.Mock).mockImplementation();
+      (fetchToken as jest.Mock).mockImplementation(() => {
+        throw new Error('Fail!');
+      });
+
+      await store.dispatch(signIn());
+      const nextState = store.getState();
+
+      expect(nextState.auth.token).toEqual(null);
+      expect(nextState.auth.isAuthenticated).toEqual(false);
+      expect(nextState.signInForm.error).toBeTruthy();
+      expect(nextState.signInForm.status).toEqual(Status.Error);
+    });
+
+    it('should set status to `loading` while waiting for reponse', () => {
+      const store = configureStore({
+        signInForm: {
+          fields: {
+            password: { value: 'test-password', isValid: true, error: null },
+            username: { value: 'test-username', isValid: true, error: null }
+          }
+        }
+      });
+
+      (validateUsername as jest.Mock).mockImplementation();
+      (validatePassword as jest.Mock).mockImplementation();
+      (fetchToken as jest.Mock).mockImplementation(() => ({
+        token: 'test'
+      }));
+
+      store.dispatch(signIn());
+      const nextState = store.getState();
+
+      expect(nextState.signInForm.status).toEqual(Status.Loading);
     });
   });
 });
