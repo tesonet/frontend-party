19c1eb6b9d346fc10d088a0dcfa54d7844734d7e
PavelMaks <pavel.maksimovic@gmail.com>
2018-09-23 15:01:16 +0300

    added jest tests

--- login/src/Login/Login.js
+++ login/src/Login/Login.js
@@ -10,13 +10,11 @@ export default class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        servers: [],
         redirect: false,
         type: 'password',
         password: '',
         username: '',
-        token: '', //supposed to be a string
-        error: ''
+        token: ''
     };
 }
 
@@ -45,8 +43,8 @@ handleSubmit() {
               this.setState({token: response.data.token, redirect: true});
             })
             .catch(error => {
-              this.setState({redirect: false, error: true}); 
-              alert('incorrect');
+              this.setState({redirect: false}); 
+              alert('Incorrect Credentials Provided');
             });
   }
 
@@ -73,10 +71,10 @@ handleSubmit() {
           <img src={logo} alt='TestioLogo' className="img-responsive center-block" />
 
             <div className="form-group" >
-              <input className="form-control form-control-lg glyphicon" type="text" onChange={this.handleChangeUsername.bind(this)} placeholder="&#57352; Username" />
+              <input className="form-control form-control-lg glyphicon" id="username" type="text" onChange={this.handleChangeUsername.bind(this)} placeholder="&#57352; Username" />
             </div>
             <div className="form-group">
-              <input className="form-control form-control-lg glyphicon" type={this.state.type} onChange={this.handleChangePassword.bind(this)} placeholder="&#57395; Password" />
+              <input className="form-control form-control-lg glyphicon" id="password" type={this.state.type} onChange={this.handleChangePassword.bind(this)} placeholder="&#57395; Password" />
               <div className="passwordVisibility"><input type="checkbox" onChange={this.handlePasswordVisibility.bind(this)} /> Show Password </div>
             </div>
             <button type="button" className="btn btn-success btn-lg btn-block" onClick={this.handleSubmit.bind(this)}>Log In</button><br/>
--- login/src/Login/Login.test.js
+++ login/src/Login/Login.test.js
@@ -7,3 +7,29 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Login />, div);
 });
+it('check default state', () => {
+  const wrapper = shallow(<Login />);
+  expect(wrapper.state().redirect).toBe(false);
+  expect(wrapper.state().username).toBe('');
+  expect(wrapper.state().password).toBe('');
+  expect(wrapper.state().token).toBe('');
+  expect(wrapper.state().type).toBe('password');
+});
+describe('Username input', () => {
+  it('should respond to change event and change the state of the Login Component', () => {
+    const wrapper = shallow(<Login />);
+    wrapper.find('#username').simulate('change', {target: {id: 'username', value: 'testUser'}});
+  expect(wrapper.state('username')).toEqual('testUser');
+  })
+})
+describe('Password input', () => {
+  it('should respond to change event and change the state of the Login Component', () => {
+    const wrapper = shallow(<Login />);
+    wrapper.find('#password').simulate('change', {target: {id: 'password', value: 'randompass'}});
+  expect(wrapper.state('password')).toEqual('randompass');
+  })
+})
+
+
+
+
