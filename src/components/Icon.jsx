33522fdab91b318d85f6178dd27bc926e9c18a4e
ZDNoFYVe <5420669+ZDNoFYVe@users.noreply.github.com>
2018-09-28 17:50:11 +0300

    Icon: Handle undefined classNames cleanly

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default ({icon, title, className}) => {
   const classes = ['fas', icon];
-  if (className !== '') {
+  if (className) {
     classes.push(className);
   }
   return <i className={classes.join(' ')} title={title} aria-hidden />;
