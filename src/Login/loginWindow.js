d893fd79109dd4bba8c9ddba9ae12b1a3a4b51e5
Renatas <renatas@elseta.com>
2018-10-16 13:36:50 +0300

    made LoginWindow component stateless

--- src/Login/loginWindow.js
+++ src/Login/loginWindow.js
@@ -4,17 +4,19 @@ import LoginForm from './loginForm';
 import Logo from './assets/images/logo.png';
 
 /*The main component for login window*/
-class LoginWindow extends Component {
-  render() {
+
+const LoginWindow = (props) => {
+//class LoginWindow extends Component {
+  //render() {
     return (<div className="login-window">
       <div className="login-window-container">
       <div className="img-container">
         <img src={Logo} className="logo" alt="logo"/>
         </div>
-        <LoginForm history={this.props.history}/>
+        <LoginForm history={props.history}/>
       </div>
     </div>);
-  }
+  //}
 }
 
 export default LoginWindow;
