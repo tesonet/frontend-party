293116b23ce124e9a7ef6d4720a3c9491a98f67a
Renatas <renatas@elseta.com>
2018-10-16 17:09:20 +0300

    sorting by name and by distance on click

--- src/Servers/ServersList.js
+++ src/Servers/ServersList.js
@@ -13,67 +13,81 @@ class ServersList extends Component {
     };
   }
 
-componentDidMount() {
-    /*Taken saved token from localstorage*/
-    const token = localStorage.getItem("testio_token");
+  componentDidMount() {
+      /*Taken saved token from localstorage*/
+      const token = localStorage.getItem("testio_token");
 
-    /*Ajax request to get servers data from server. Passing token into header*/
-    axios.get('http://playground.tesonet.lt/v1/servers', {
-  //      axios.get('/data.json', {
-      headers: {
-        "Authorization": token
-      }
-    }).then((res) =>{
-      let sorted_array = res.data.sort((a, b) => a.name.localeCompare(b.distance));
-      this.setState({
-        server_error: false,
-        data: sorted_array,
-        request_finished: true});
-    }).catch((error) => {
-      console.log(error);
-      this.setState({server_error: true, error_message: "Server error"});
-    })
-}
-
-
-/*
-Checking receved data or error in server_error.
-  3 cases:
-        1.If get server error;
-        2. If server server status is OK but where is no data;
-        3. If server status is Ok and have some data to show;
-These function return is in the render method.
-*/
-renderServerstable() {
-  if (this.state.server_error === true) {
-    return (<p>Server error</p>)
-  } else if (this.state.request_finished === true && this.state.data.length === 0) {
-    return (<p>No data</p>)
-  } else {
-    return (<div>
-      {
-        this.state.data.map((data, i) => (<div className="servers-row" key={i}>
-          <p>{data.name}</p>
-          <p>{data.distance}</p>
-        </div>))
-      }
-    </div>)
+      /*Ajax request to get servers data from server. Passing token into header*/
+      axios.get('http://playground.tesonet.lt/v1/servers', {
+        headers: {
+          "Authorization": token
+        }
+      }).then((res) =>{
+        this.setState({
+          server_error: false,
+          data: this.sortBy(res.data, "name"), /*Passing data sorted by name*/
+          request_finished: true});
+      }).catch((error) => {
+        console.log(error);
+        this.setState({server_error: true, error_message: "Server error"});
+      })
   }
-}
 
-render() {
-  return (<div>
-    <div className="servers-header">
-      <p>SERVER</p>
-      <p>DISTANCE</p>
-    </div>
-    <div className="servers-container">
-      {this.renderServerstable()}
-    </div>
+  /*
+    Sorting array by passed arguments
+    Passing array of objects and sorting argument.
+  .*/
+  sortBy = (arr, prop) => {
+    return arr.sort((a,b) => (a[prop] > b[prop]) ? 1 : ((b[prop] > a[prop]) ? -1 : 0));
+  }
 
-  </div>);
-}
+  /*On click sorting servers by name*/
+  sortingOnServerClick = () => {
+    this.setState({data: this.sortBy(this.state.data, "name")});
+  }
 
+  /*On click sorting servers by distance*/
+  sortingOnDistanceClick = () => {
+    this.setState({data: this.sortBy(this.state.data, "distance")});
+  }
+
+  /*
+  Checking receved data or error in server_error.
+    3 cases:
+          1.If get server error;
+          2. If server server status is OK but where is no data;
+          3. If server status is Ok and have some data to show;
+  These function return is in the render method.
+  */
+  renderServerstable() {
+    if (this.state.server_error === true) {
+      return (<p>Server error</p>)
+    } else if (this.state.request_finished === true && this.state.data.length === 0) {
+      return (<p>No data</p>)
+    } else {
+      return (<div>
+        {
+          this.state.data.map((data, i) => (<div className="servers-row" key={i}>
+            <p>{data.name}</p>
+            <p>{data.distance}</p>
+          </div>))
+        }
+      </div>)
+    }
+  }
+
+  render() {
+    return (<div>
+      <div className="servers-header">
+        <p onClick={this.sortingOnServerClick}>SERVER</p>
+        <p onClick={this.sortingOnDistanceClick}>DISTANCE</p>
+      </div>
+      <div className="servers-container">
+        {this.renderServerstable()}
+      </div>
+
+    </div>);
+  }
 }
 
 export default ServersList
