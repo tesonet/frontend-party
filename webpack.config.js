c38de89dc96d1927aeaef1b0be268ddfbd6337f3
egidijus <e.jakas@adform.com>
2018-11-07 19:34:56 +0200

    added dummy application with basic configs for linting/bundling/etc

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
