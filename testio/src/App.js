9a23a0361b5d9e79feffe4252a54a5c66653a359
juozasm <juozas.masiliunas@gmail.com>
2018-11-18 19:05:49 +0200

    basic routing added

--- testio/src/App.js
+++ testio/src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
+import Home from './routes/Home';
+import Login from './routes/Login';
 class App extends Component {
   render() {
     return (
-      <h1>Hello world</h1>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/login" component={Login}/>
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
