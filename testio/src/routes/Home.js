ee64f12649dc2d3773ec073beec505e8e06bb8b2
juozasm <juozas.masiliunas@gmail.com>
2018-11-18 22:12:51 +0200

    auth  funcionality, login logout, store data and token to local storage

--- testio/src/routes/Home.js
+++ testio/src/routes/Home.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+    state = {
+        data:'',
+        loaded:false
+    }
+    logout = ()=>{
+        localStorage.removeItem('data');
+        localStorage.removeItem('jwt-token');
+        this.props.history.push('/login'); 
+    }
+    getData = ()=>{  
+        let token=localStorage.getItem('jwt-token')
+        console.log(token)
+        if(!token){
+            this.props.history.push('/login')
+        }else{
+            let data=localStorage.getItem('data')
+            if(data){
+                this.setState({data:JSON.parse(data)})
+            }else{
+                let config = {
+                    method: 'GET',
+                    headers: { 'authorization':token}
+                }
+                    // request to http://playground.tesonet.lt/v1/servers
+                    fetch('http://playground.tesonet.lt/v1/servers', config).then(
+                        res=>{
+                            if(res.ok) {
+                             res.json().then(res=>{
+                                 console.log(res)
+                                 localStorage.setItem('data', JSON.stringify(res));
+                                 this.setState({data:res})
+                             })
+                            } else {
+                            localStorage.removeItem('jwt-token');
+                            localStorage.removeItem('data');
+                            this.props.history.push('/login');
+                            throw Error(`Request rejected with status ${res.status}`);      
+                            }
+                        }).catch((e)=>{
+                            console.log(e);         
+                        })
+            }
+                }
+    }
+    componentWillMount(){
+        this.getData();
+    }
+    componentDidMount(){
+        this.setState({loaded:true});
+    }
     render() {
         return (
             <div>
                 Home
+                <button
+                onClick={this.logout}
+                >Logout</button>
             </div>
+            
         )
     }
 }
--- testio/src/routes/Login.js
+++ testio/src/routes/Login.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+    state = {
+        username:'tesonet',
+        password:'partyanimal'
+    }
+    login = ()=>{
+                let config = {
+                    method: 'POST',
+                    headers: { 'Content-Type':'application/x-www-form-urlencoded' },
+                    body: `username=tesonet&password=partyanimal`
+                }
+                // request to http://playground.tesonet.lt/v1/tokens
+                fetch('http://playground.tesonet.lt/v1/tokens', config).then(
+                    res=>{
+                        if(res.ok) {
+                         res.json().then(res=>{
+                            console.log(res.token);
+                            localStorage.setItem('jwt-token', res.token);
+                            this.props.history.push('/');
+                         })
+                        } else {
+                          throw Error(`Request rejected with status ${res.status}`);
+                        }
+                    }).catch((e)=>{
+                        console.log(e);         
+                    })
+    }
+    componentWillMount(){
+       if (localStorage.getItem('jwt-token')) return this.props.history.push('/');
+    }
     render() {
         return (
             <div>
                 Login
+                <button
+                onClick={this.login}
+                >Login</button>
             </div>
         )
     }
