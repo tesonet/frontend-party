98cc52cf4a0ffc6ea22826db606627ac0e241d33
Karolis Juškauskas <karolis.juskauskas95@gmail.com>
2019-01-30 18:25:54 +0200

    Feature: Login view structure

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -13,8 +13,26 @@ class Login extends React.Component {
 
   public render() {
     return (
-      <div>
-        Login
+      <div className="login">
+        <div className="login__wrap">
+          <div className="container">
+            <form className="row form">
+              <div className="col-10 offset-1 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+                <div className="from__input form-group">
+                  <div className="form__icon"></div>
+                  <input className="form-control" type="text" value={this.login}/>
+                </div>
+                <div className="from__input form-group">
+                  <div className="form__icon"></div>
+                  <input className="form-control" type="password" value={this.password}/>
+                </div>
+                <div className="form__submit">
+                  <button className="btn btn-primary btn-block" type="submit" >Log In</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
