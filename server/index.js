275430cf5e66cd5745383a97026a9386ae0572e2
arnoldasdev <arnoldasdeveloper@gmail.com>
2019-06-30 04:13:52 +0300

    Express server added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
 import path from 'path';
+import express from 'express';
 
-console.log('babel-ok');
+const PORT = process.env.PORT || 5000;
+const pathname = path.resolve(`${__dirname}/../client/build`);
+
+console.log({ pathname });
+
+const start = (pid) => {
+  const app = express();
+  app.use('/', express.static(pathname));
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT} pid: ${pid}`));
+};
+
+if (!module.parent) start(process.pid);
+
+export default { start };
