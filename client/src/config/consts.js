b48fed82f3eda262050be5f9ccd4b777dcebfed0
arnoldasdev <arnoldasdeveloper@gmail.com>
2019-07-01 00:28:21 +0300

    Token class, config added

---
+++ client/src/config/consts.js
@@ -0,0 +1,2 @@
+export const URL_TOKENS = 'http://playground.tesonet.lt/v1/tokens';
+export const URL_SERVERS = 'http://playground.tesonet.lt/v1/servers';
---
+++ client/src/modules/token.js
@@ -0,0 +1,23 @@
+class Token {
+  constructor() {
+    this.user = this.find();
+  }
+
+  fetch = () => {
+
+  };
+
+  find = () => {
+
+  };
+
+  save = () => {
+
+  };
+
+  remove = () => {
+
+  };
+}
+
+export default Token;
