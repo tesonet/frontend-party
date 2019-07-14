9486f1f48217457a28675c5771eb711b24982371
budaman <budamantvydas@gmail.com>
2019-07-14 11:14:55 +0300

    setup redux

---
+++ _reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import testReducer from "./test";
+
+const reducers = combineReducers({
+  testReducer
+});
+
+export default reducers;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "../_reducers";
+
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+const middlewareThunk = applyMiddleware(thunkMiddleware);
+
+const store = createStore(rootReducer, undefined, middlewareThunk);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
