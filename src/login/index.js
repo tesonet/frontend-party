8613323935611926befd48db5adb45db0b11367f
budaman <budamantvydas@gmail.com>
2019-07-16 04:58:26 +0300

    Login component UI

--- src/login/index.js
+++ src/login/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import testioLogo from "../../assets/testio.png";
 import Background from "../background";
 
 import styles from "./styles.css";
@@ -7,7 +8,26 @@ import styles from "./styles.css";
 const Login = () => {
   return (
     <Background>
-      <div className={styles.container}>login</div>
+      <div className={styles.container}>
+        <form className={styles.form}>
+          <img
+            className={styles.testioLogo}
+            alt="testio logo"
+            src={testioLogo}
+          />
+          <input
+            className={`${styles.input} ${styles.userInput}`}
+            placeholder="Username"
+          />
+          <input
+            className={`${styles.input} ${styles.passwordInput}`}
+            placeholder="Password"
+          />
+          <button className={styles.submit} type="submit">
+            Log In
+          </button>
+        </form>
+      </div>
     </Background>
   );
 };
