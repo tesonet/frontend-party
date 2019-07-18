ae0def702be40141604ff9a71c251572a6b75293
budaman <budamantvydas@gmail.com>
2019-07-18 21:27:20 +0300

    server list component

--- src/components/server-list/ServerList.js
+++ src/components/server-list/ServerList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 
 import ServerListHeader from "../server-list-header";
@@ -10,9 +10,28 @@ const ServerList = ({ getServerListAction, serverList }) => {
     getServerListAction();
   }, [getServerListAction]);
   return (
-    <div className={styles.container}>
+    <Fragment>
       <ServerListHeader />
-    </div>
+      <div>
+        <div className={`${styles.tableRow} ${styles.tableHeader}`}>
+          <h6 className={`${styles.text} ${styles.tableHeaderText}`}>server</h6>
+          <h6 className={`${styles.text} ${styles.tableHeaderText}`}>
+            distance
+          </h6>
+        </div>
+        <div className={styles.tableBody}>
+          {serverList.map(server => (
+            <div
+              key={server.name + server.distance}
+              className={styles.tableRow}
+            >
+              <p className={styles.text}>{server.name}</p>
+              <p className={styles.text}>{server.distance}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Fragment>
   );
 };
 
