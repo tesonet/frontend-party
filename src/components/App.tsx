ea93fef3bbdd88fa242eae67bac2971980391705
Vytautas Ziliukas <vytautas.ziliukas@skycop.com>
2019-08-07 21:40:39 +0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>Hello to Frontend Party!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
