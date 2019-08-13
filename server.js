03cc0d498b9d5f1000b50b84c39f62f39a7aed13
Vytautas Ziliukas <vytautas.ziliukas@skycop.com>
2019-08-13 11:39:29 +0300

    Fixed path

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const app = express();
 
+const directoryPath = __dirname + "/dist";
 // Allow cros origin domain
 app.use(cors());
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(directoryPath));
 
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
+    res.sendFile(path.resolve(directoryPath, "index.html"));
 });
 
 app.listen(port);
