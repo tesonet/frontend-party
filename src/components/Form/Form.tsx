bed9936be2f95a97521bec8a5c95534ba526c97d
Justinas Reigis <justinas.reigis@gmail.com>
2019-08-21 20:00:30 +0100

    remove unused prop

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,8 +6,8 @@ import Input from '../Input';
 const Form: React.FC = () => {
   return (
     <form action="submit" className={'w-full max-w-sm'}>
-      <Input icon={'user'} placeholder={'Username'} type={'text'} />
-      <Input icon={'lock'} placeholder={'Password'} type={'password'} />
+      <Input placeholder={'Username'} type={'text'} />
+      <Input placeholder={'Password'} type={'password'} />
       <Button text="Log In" />
     </form>
   );
