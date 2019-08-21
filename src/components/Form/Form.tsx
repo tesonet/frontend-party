2a6baee10cf69c3e59c07599b1907717fbe57990
Justinas Reigis <justinas.reigis@gmail.com>
2019-08-21 19:55:44 +0100

    cleanup

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 import Button from '../Button';
 import Input from '../Input';
-import userIcon from '../../assets/icons/user.svg';
 
 const Form: React.FC = () => {
   return (
     <form action="submit" className={'w-full max-w-sm'}>
-      <Input icon={userIcon} placeholder={'Username'} type={'text'} />
+      <Input icon={'user'} placeholder={'Username'} type={'text'} />
       <Input icon={'lock'} placeholder={'Password'} type={'password'} />
       <Button text="Log In" />
     </form>
