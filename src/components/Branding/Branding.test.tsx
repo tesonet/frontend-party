b89d6d4e205dc73e1429fe52160f598c5b695271
Justinas Reigis <justinas.reigis@gmail.com>
2019-08-30 13:16:49 +0100

    cleanup: remove unused vars

--- src/components/Branding/Branding.test.tsx
+++ src/components/Branding/Branding.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 import React from 'react';
-import { StaticRouter } from 'react-router';
 
 import Branding from './Branding';
 import { IProps } from './Branding.interface';
