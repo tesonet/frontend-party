c77306fb8a565628b97822c25694c230b054cba4
Justinas Reigis <justinas.reigis@gmail.com>
2019-09-01 11:53:11 +0100

    remove nav links

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import LoginPage from './components/LoginPage';
 import ServerList from './components/ServerList';
 
 const App: React.FC = () => (
   <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/server/">Server</Link>
-          </li>
-        </ul>
-      </nav>
-
+    <>
       <Route path="/" exact component={LoginPage} />
       <Route path="/server/" component={ServerList} />
-    </div>
+    </>
   </Router>
 );
 
