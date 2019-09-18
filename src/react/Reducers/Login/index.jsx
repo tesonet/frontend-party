3ccb7fa43095a795feac337a41c1b76c4462d598
Eugenijus <Eugenijus07@gmail.com>
2019-09-18 12:40:48 +0300

    Update index.jsx

--- src/react/Reducers/Login/index.jsx
+++ src/react/Reducers/Login/index.jsx
@@ -3,7 +3,6 @@ import {LOGIN_REQUEST, LOGOUT} from '../../Constants/Login/index';
 export default function login(state, action) {
     switch (action.type){
         case LOGIN_REQUEST:
-            console.log('ds');
             return {...state, token: action.token};
         case LOGOUT:
             return {token: null};
