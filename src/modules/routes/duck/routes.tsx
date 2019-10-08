684407e82c62217a3cd53387b29443472d10f9a9
Mikalojus Galminas <mikalojus.galminas@zedge.net>
2019-10-08 15:36:22 +0300

    react-loadable replaced with react.lazy

--- src/modules/routes/duck/routes.tsx
+++ src/modules/routes/duck/routes.tsx
@@ -1,6 +1,8 @@
 import { Loading } from '@components/Loading';
+import { LoginContainer } from '@modules/auth/LoginContainer';
 import { withAuthAccess } from '@modules/auth/withAuthAccess';
-import * as Loadable from 'react-loadable';
+import { ServerList } from '@modules/servers/ServerList';
+import * as React from 'react';
 import { RouteConfig } from 'react-router-config';
 import { compose } from 'redux';
 
@@ -11,23 +13,19 @@ import { routePath } from './actions';
 export const routes: RouteConfig[] = [
   {
     path: routePath.login,
-    component: Loadable({
-      loader: () =>
-        import('@modules/auth/LoginContainer').then(
-          ({ LoginContainer }) => LoginContainer,
-        ),
-      loading: Loading,
-    }),
+    component: () => (
+      <React.Suspense fallback={<Loading />}>
+        <LoginContainer />
+      </React.Suspense>
+    ),
   },
   {
     path: routePath.root,
-    component: Loadable({
-      loader: () =>
-        import('@modules/servers/ServerList').then(({ ServerList }) =>
-          compose(withAuthAccess)(ServerList),
-        ),
-      loading: Loading,
-    }),
+    component: compose(withAuthAccess)(() => (
+      <React.Suspense fallback={<Loading />}>
+        <ServerList />
+      </React.Suspense>
+    )),
   },
   {
     path: routePath.error,
