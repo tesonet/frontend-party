e078433abf91fe1b5df199078503bf6dc7dfff84
Karolis Vilpisauskas <karolis.vilpisauskas@gmail.com>
2019-11-12 21:23:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Testio.</h1>
+    </div>
+  );
+}
+
+export default App;
