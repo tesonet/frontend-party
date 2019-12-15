bc88a70b1160c506637578a692959d503f0bfaa3
Mantas Giniunas <mantasginiunas@Mantass-MacBook-Pro.local>
2019-12-15 12:19:50 +0200

    use null instead of empty string

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -34,8 +34,8 @@ function Login() {
       <Form
         onSubmit={({ name, password }) => dispatch(auth.actions.authenticate(name, password))}
         initialValues={{
-          name: '',
-          password: '',
+          name: null,
+          password: null,
         }}
         render={({ handleSubmit }) => (
           <>
