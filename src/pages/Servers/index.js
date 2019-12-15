100454f4b120a4b0cf7950cbb5627ed1f0e86336
Mantas Giniunas <mantasginiunas@Mantass-MacBook-Pro.local>
2019-12-15 15:32:45 +0200

    add servers sorting by name and distance

--- src/pages/Servers/index.js
+++ src/pages/Servers/index.js
@@ -8,22 +8,26 @@ import logoutIcon from '<assets>/icons/logout.svg';
 
 import './style.scss';
 
+const compareByName = ({ name: a }, { name: b }) => (a > b ? 1 : -1);
+const compareByDistance = ({ distance: a }, { distance: b }) => (a > b ? 1 : -1);
+
 function Servers() {
   const dispatch = useDispatch();
   const serversData = useSelector(servers.selectors.getServersData);
   const isFetching = useSelector(servers.selectors.isFetching);
-
+  const [displayedServers, setDisplayedServers] = useState([]);
   useEffect(() => {
     if (!serversData) {
       dispatch(servers.actions.requestServers());
     }
-  });
-  console.log(serversData);
-  const [displayedServers, setDisplayedServers] = useState(serversData);
-
+    if (!displayedServers.length && serversData) {
+      setDisplayedServers(serversData);
+    }
+  }, [serversData, dispatch, displayedServers]);
+  console.log(displayedServers);
   return (
     <div className="servers container">
-      {!isFetching && <Loader />}
+      {isFetching && <Loader />}
       <div className="servers__header">
         <h1>
           testio<mark>.</mark>
@@ -36,6 +40,30 @@ function Servers() {
           <img src={logoutIcon} alt="logout" />
           Logout
         </button>
+      </div>
+      <div className="servers__content">
+        <ul className="servers__content-list">
+          <li className="server_content-header">
+            <button
+              onClick={() => setDisplayedServers([...serversData.sort(compareByName)])}
+              type="button"
+            >
+              Server
+            </button>
+            <button
+              onClick={() => setDisplayedServers([...serversData.sort(compareByDistance)])}
+              type="button"
+            >
+              Distance
+            </button>
+          </li>
+          {displayedServers.map(({ name, distance }, index) => (
+            <li key={index}>
+              <span>{name} </span>
+              <span>{distance}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
