a31ec38c10b5319831f28c7b52a2e6f679f121de
IgnasAusiejus <ignas@zenitech.co.uk>
2020-01-02 01:50:46 +0200

    edit eslint to ignore jest methods

--- src/pages/servers/table/table.test.js
+++ src/pages/servers/table/table.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { createMockStore } from 'redux-test-utils';
 import Header from './tableHeader';
 import TableRow from './tableRow';
 
