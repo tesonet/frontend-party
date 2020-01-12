df49f871a44cd4cab2bcdcc6069d23a7de5a9869
Linas Vainoras <hello@linas.dev>
2020-01-12 23:45:57 +0200

    basic webpack and react setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require("path"),
+  webpack = require("webpack"),
+  HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: ["./src/index.tsx"],
+    vendor: ["react", "react-dom"]
+  },
+  output: {
+    publicPath: "http://localhost:3000",
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      {
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    publicPath: "/",
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
