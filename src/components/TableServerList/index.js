b158b3e1119ce20db55658eb0cf3533c49cc0cc8
Evaldas <evabur@icloud.com>
2020-01-18 11:30:30 +0000

    filtering by country & distance

--- src/components/TableServerList/index.js
+++ src/components/TableServerList/index.js
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { onServerListDataFetch } from "../../thunks/dataThunks/fetchDataThunks";
+import Triangle from "../../assets/svg/triangle.svg";
+import sort from "../../utils/sort";
 
 const Tr = ({ server }) => (
   <tr>
@@ -10,7 +12,37 @@ const Tr = ({ server }) => (
 )
 
 const TableServerList = ({ serverList, loading, handleFetch }) => {
-  console.log(serverList);
+  const [sortByName, setSortByName] = useState({
+    isActive: false,
+    asc: false
+  });
+  const [sortByDistance, setSortByDistance] = useState({
+    isActive: false,
+    asc: false
+  });
+
+  let filteredList = serverList;
+  
+  const handleFilter = (e) => {
+    let pickedFilter = e.target.name;
+
+    if(pickedFilter === "filterByDistance") {
+      setSortByName({isActive: false, asc: false});
+      setSortByDistance({isActive: true, asc: !sortByDistance.asc});
+
+      sortByDistance.asc ? filteredList = sort(filteredList, "distance", "asc") : filteredList = sort(filteredList, "distance", "desc");;
+    }
+
+    if(pickedFilter === "filterByName") {
+      setSortByDistance({isActive: false, asc: false})
+      setSortByName({isActive: true, asc: !sortByName.asc})
+
+      sortByName.asc ? filteredList = sort(filteredList, "name", "asc") : filteredList = sort(filteredList, "name", "desc");;
+    }
+
+    e.preventDefault();
+  }
+
 
   useEffect(() => {
     handleFetch();
@@ -20,12 +52,28 @@ const TableServerList = ({ serverList, loading, handleFetch }) => {
     <table className="server-list-table">
       <thead className="server-list-table__head">
         <tr>
-          <th>Server</th>
-          <th>Distance</th>
+          <th>
+            <button onClick={handleFilter} name="filterByName">
+              Server
+              <img 
+                src={Triangle} 
+                style={{transform: `${sortByName.asc ? "rotate(180deg)" : "rotate(0deg)"}`, opacity: `${sortByName.isActive ? 1 : 0.5}`}} 
+                alt="sorting icon"/>
+            </button>
+          </th>
+          <th>
+            <button onClick={handleFilter} name="filterByDistance">
+              Distance
+              <img 
+                src={Triangle} 
+                style={{transform: `${sortByDistance.asc ? "rotate(180deg)" : "rotate(0deg)"}`, opacity: `${sortByDistance.isActive ? 1 : 0.5}`}} 
+                alt="sorting icon"/>
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody className="server-list-table__body">
-        {serverList.map(server => <Tr key={server.name + server.distance} server={server}/>)}
+        {loading ? <tr><td>Loading</td></tr> : filteredList.map(server => <Tr key={server.name + server.distance} server={server}/>)}
       </tbody>
     </table>
   );
@@ -38,3 +86,4 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TableServerList);
+
---
+++ src/utils/sort.js
@@ -0,0 +1,9 @@
+export default (arr, property, type) => {
+    let sortedArray = arr.sort((a, b) => {
+        return a[property] !== b[property] ? a[property] < b[property] ? 1 : -1 : 0
+    })
+
+    type === "desc" && sortedArray.reverse();
+    
+    return sortedArray;
+}
