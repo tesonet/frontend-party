3809fbd9c6f8b4bde17d87b8a100c5665b43b9b5
Justas <justas0211+tesonet@gmail.com>
2020-02-09 20:03:49 +0000

    Install styled-components, styled-normalize and add GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { Normalize } from "styled-normalize";
+import { createGlobalStyle } from "styled-components";
 import App from "./App";
 import store from "./store";
 
+const GlobalStyles = createGlobalStyle`
+  @import url("https://fonts.googleapis.com/css?family=Roboto:300,700");
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Fragment>
+    <Normalize />
+    <GlobalStyles />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Fragment>,
   rootElement
 );
