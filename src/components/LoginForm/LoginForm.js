37f64a892bf5b04579ecc94f3560318c6dffb6c0
Justas <justas0211+tesonet@gmail.com>
2020-02-09 20:09:49 +0000

    Style Login page

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,18 +1,75 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import styled from "styled-components";
 import { onLogin, loginClearError } from "../../actions/authActions";
+import User from "../Icons/User";
+import Lock from "../Icons/Lock";
 
 const validate = values => {
   let errors = {};
   if (!values.username) {
-    errors.username = "Required";
+    errors.username = "Required field";
   }
   if (!values.password) {
-    errors.password = "Required";
+    errors.password = "Required field";
   }
   return errors;
 };
+
+const StyledForm = styled(Form)`
+  display: flex;
+  flex-direction: column;
+  height: 230px;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 363px;
+`;
+
+const StyledButton = styled.button`
+  background-color: #9fd533;
+  border: none;
+  border-radius: 10px;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 16px;
+  height: 58px;
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: #86b300;
+    cursor: pointer;
+  }
+`;
+
+const StyledInputField = styled(Field)`
+  border: none;
+  height: 58px;
+  font-weight: 300;
+  letter-spacing: 0.4px;
+
+  ::placeholder {
+    color: #b3b3b3;
+    font-weight: 300;
+    letter-spacing: 0.4px;
+  }
+`;
+
+const StyledInputContainer = styled.div`
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+const StyledErrorMessage = styled(ErrorMessage)`
+  color: #ff000094;
+`;
+
+const StyledAuthError = styled.div`
+  height: 20px;
+  color: #fff;
+  text-align: center;
+`;
 
 const LoginForm = () => {
   const isFetching = useSelector(({ auth }) => auth.isFetching);
@@ -28,17 +85,35 @@ const LoginForm = () => {
         return validate(values);
       }}
       onSubmit={values => dispatch(onLogin(values))}
+      onBlur={(...args) => console.log(args)}
     >
-      <Form>
-        <Field type="username" name="username" placeholder="Username" />
-        <ErrorMessage name="username" component="div" />
-        <Field type="password" name="password" placeholder="Password" />
-        <ErrorMessage name="password" component="div" />
-        {authError && "Login failed. Try again."}
-        <button type="submit" disabled={isFetching}>
-          Login
-        </button>
-      </Form>
+      <StyledForm>
+        <StyledInputContainer>
+          <User />
+          <StyledInputField
+            type="username"
+            name="username"
+            placeholder="Username"
+          />
+          <StyledErrorMessage name="username" component="div" />
+        </StyledInputContainer>
+
+        <StyledInputContainer>
+          <Lock />
+          <StyledInputField
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <StyledErrorMessage name="password" component="div" />
+        </StyledInputContainer>
+        <StyledButton type="submit" disabled={isFetching}>
+          {isFetching ? "Loading..." : "Log In"}
+        </StyledButton>
+        <StyledAuthError>
+          {authError && "Login failed. Please try again."}
+        </StyledAuthError>
+      </StyledForm>
     </Formik>
   );
 };
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,17 +1,47 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import background from "../../assets/png/background.png";
+import logoLight from "../../assets/png/logo-light.png";
+
+// TODO add overlay which works with IE11
+const StyledBackground = styled.div`
+  background: url(${background});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+`;
+
+const StyledLoginContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  margin: 0 16px;
+`;
+
+const StyledLogo = styled.img`
+  min-height: 64px;
+  min-width: 246px;
+  margin-bottom: 69px;
+`;
 
 const Login = () => {
   const loggedIn = useSelector(({ auth }) => auth.loggedIn);
 
   return loggedIn ? (
-    <Redirect to="/servers" />
+    <Redirect to="/" />
   ) : (
-    <div>
-      <LoginForm />
-    </div>
+    <StyledBackground>
+      <StyledLoginContainer>
+        <StyledLogo src={logoLight} />
+        <LoginForm />
+      </StyledLoginContainer>
+    </StyledBackground>
   );
 };
 
