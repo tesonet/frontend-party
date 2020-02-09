f01b215ff3d75f6de299d2a3535c4112ad209831
Justas <justas0211+tesonet@gmail.com>
2020-02-09 11:26:55 +0000

    Add utils for using localStorage

---
+++ src/utils/localStorage/localStorage.js
@@ -0,0 +1,4 @@
+export const getFromLocalStorage = key => localStorage.getItem(key);
+export const setToLocalStorage = (key, value) =>
+  localStorage.setItem(key, value);
+export const removeFromLocalStorage = key => localStorage.removeItem(key);
