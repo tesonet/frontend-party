6f6beefd2a10aa289917f784e9b360eafc5e6bbf
Justas <justas0211+tesonet@gmail.com>
2020-02-10 18:41:17 +0000

    Polyfill fetch for IE11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+import "whatwg-fetch";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
