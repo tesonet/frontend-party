700feffd8bcaaa11fec4ad14e8103a6c1c192fd0
Justas <justas0211+tesonet@gmail.com>
2020-02-10 11:29:53 +0000

    Add exact to routes and fix import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import GlobalStyles from "/GlobalStyles";
+import GlobalStyles from "./GlobalStyles";
 import store from "./store";
 
 const rootElement = document.getElementById("root");
