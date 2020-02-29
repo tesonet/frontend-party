4da1ead1e597d2d472c3cebe92f11b1fcff6f3bc
Lukas Laurinavicius <lukaslaurina@gmail.com>
2020-02-29 15:50:13 +0200

    components/login background

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import backgroundImg from './assets/background.png';
 
 const Background = styled.div`
   height: 100vh;
-  background-image: url(${backgroundImg});
+  background: linear-gradient(rgba(11, 15, 39, 0.8), rgba(11, 15, 39, 0.8)), url(${backgroundImg});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 `;
 
+const BG = styled.div`
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(11, 15, 39);
+  opacity: 0.8;
+  z-index: 1000;
+`;
+
 const App = () => <Background />;
+
 export default App;
