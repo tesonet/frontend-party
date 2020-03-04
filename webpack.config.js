27c1d4d9422997cb730961526af275c4e6765214
Lukas Laurinavicius <lukaslaurina@gmail.com>
2020-03-04 22:12:07 +0200

    config/ small fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, '/build'),
+    path: path.join(__dirname, '/build'),
     filename: 'bundle.js',
   },
   module: {
