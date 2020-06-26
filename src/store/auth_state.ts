c538957e4114afcde0c6bb0dc7e3a660bff04df4
Yegor Levandovskyi <levandovskyi98@gmail.com>
2020-06-26 23:23:18 +0300

    removed redundant file

--- src/store/auth_state.ts
+++
@@ -1,9 +0,0 @@
-import { createStore, Action } from 'redux'
-import { ReducerAction } from 'react'
-
-function authStatus(state = false, action: Action) {
-  switch (action.type) {
-    case 'LOG-IN':
-      break
-  }
-}
