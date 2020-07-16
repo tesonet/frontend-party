d7c970a5f7fa546a3cf2d8e76e9613c2f99303ab
mickonis <mickonis.l@gmail.com>
2020-07-16 18:50:20 +0300

    added button componenet, input error, variables

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-export const Input = ({ value, onChange, placeholder, name, type }) => (
+export const Input = ({
+  value,
+  onChange,
+  placeholder,
+  name,
+  type,
+  icon,
+  error,
+}) => (
   <div className="input__container">
+    <div
+      className="input__icon"
+      style={{
+        background: `url('${icon}') no-repeat center center / 16px`,
+      }}
+    />
     <input
       className="input__element"
       name={name ? name : placeholder}
@@ -10,5 +24,6 @@ export const Input = ({ value, onChange, placeholder, name, type }) => (
       value={value}
       onChange={(e) => onChange(e.target.value)}
     />
+    {error && <div className="input__error">{error}</div>}
   </div>
 );
--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,24 +1,63 @@
 import React, { Component } from "react";
 import { Input } from "../../Components/Input/Input";
+import { Button } from "../../Components/Button/button";
+import userIcon from "../../assets/img/user.svg";
+import lockIcon from "../../assets/img/lock.svg";
+import logo from "../../assets/img/logo@2x.png";
 
 class Login extends Component {
   state = {
     username: "",
     password: "",
+    usernameError: "",
+    passwordError: "",
   };
+
+  validateForm() {
+    const { username, password } = this.state;
+    let valid = true;
+    this.setState({ usernameError: "", passwordError: "" });
+
+    if (username === "") {
+      this.setState({ usernameError: "Please enter your username" });
+      valid = false;
+    }
+
+    if (password === "") {
+      this.setState({ passwordError: "Please enter your password" });
+      valid = false;
+    }
+
+    if (!valid) return;
+  }
+
   render() {
-    const { username } = this.state;
+    const { username, password, usernameError, passwordError } = this.state;
 
     return (
       <div className="login-page">
         <div className="login__wrap">
-          <div className="login__logo"></div>
+          <div className="login__logo">
+            <img src={logo} alt="Testio logo" width="246" height="64" />
+          </div>
           <div className="login__form">
             <Input
               placeholder="Username"
+              type="text"
+              icon={userIcon}
               value={username}
               onChange={(username) => this.setState({ username })}
+              error={usernameError}
             />
+            <Input
+              placeholder="Password"
+              type="password"
+              icon={lockIcon}
+              value={password}
+              onChange={(password) => this.setState({ password })}
+              error={passwordError}
+            />
+            <Button label="Log In" onClick={() => this.validateForm()} />
           </div>
         </div>
       </div>
