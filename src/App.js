e29373a34561490722e0c5501b988a92db95aba6
mickonis <mickonis.l@gmail.com>
2020-07-16 17:02:24 +0300

    router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="testio-app">
-      <span>testio</span>
+      <Router>
+        <Switch>
+          {/* <Route exact path="/login" component={Login} /> */}
+          {/* <Route exact path="/server-list" component={ServerList} /> */}
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
